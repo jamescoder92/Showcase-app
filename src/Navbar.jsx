4b64408ad58456d35af0cb6b276fe229c53195da
jamescoder92 <kusimbajames92@gmail.com>
2026-05-17 23:38:25 +0300

    changes in navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Navlink } from "react-router-dom";
+import { link } from "react-router-dom";
 
 function Navbar() {
   return (
@@ -6,9 +6,9 @@ function Navbar() {
         <h1 className="navbar-title">Nakumatt Electronics</h1>
 
         <div className="navbar-links"> 
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/shop">Shop</NavLink>
-            <NavLink to="/admin">Admin Portal</NavLink>
+            <Link to="/">Home</Link>
+            <Link to="/shop">Shop</Link>
+            <Link to="/admin">Admin Portal</Link>
 
         </div>
     </nav>
