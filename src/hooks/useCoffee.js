a7db9e79cd0eca7c7e07b945c31db584f7ec8ca6
jamescoder92 <kusimbajames92@gmail.com>
2026-05-17 22:21:29 +0300

    code on useCoffe.js

---
+++ src/hooks/useCoffee.js
@@ -0,0 +1,16 @@
+import { useState, useEffect } from 'react';
+
+function useCoffee() {
+  const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:3001/products')
+        .then(response => response.json())
+        .then(data => setProducts(data))
+        .catch(error => console.error('Error fetching products:', error));
+    }, []);
+
+  return products;
+}
+
+export default useCoffee;
